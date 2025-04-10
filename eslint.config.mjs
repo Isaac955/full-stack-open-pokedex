5174983d97b98a805153889b86e77c805a1e17d5
Isaac Serhane <isaacserhane95@gmail.com>
2025-04-10 09:32:31 +0200

    modifs eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
 
 export default [
   {
-    files: ["src/**/*.js"],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       sourceType: "module",
       ecmaVersion: 2022,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
       globals: {
         ...globals.browser,
         ...globals.node,
@@ -14,20 +20,32 @@ export default [
         __dirname: "readonly",
       },
     },
+    plugins: {
+      react: reactPlugin,
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
     rules: {
       ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
       "react/react-in-jsx-scope": "off",
+      "no-undef": "error",
+      "react/prop-types": "off",
     },
   },
   {
-    files: ["*.js"],
+    files: ["**/*.jest.spec.{js,jsx}"],
     languageOptions: {
-      sourceType: "commonjs",
-      ecmaVersion: 2022,
       globals: {
-        ...globals.node,
-        process: "readonly",
-        __dirname: "readonly",
+        ...globals.jest,
+        jest: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
       },
     },
   },
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const express = require('express')
 const app = express()
-
 app.listen(3000, () => console.log('app is now running'))
